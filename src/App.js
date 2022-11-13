d446730882061f9e72791f80c9c335c30caa993a
mlandauro <micaela.landauro@gmail.com>
2022-11-13 04:10:12 -0600

    search bar 1.0

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './component/home';
 import Search from './component/search';
 import Create from './component/create';
+import SearchBar from "./component/SearchBar";
 import './App.css';
+//import PlaceData from "./Data.json";
   
-class App extends Component {
+class App extends Component{
   render() {
     return (
        <Router>
            <div className="App">
             <ul className="App-header">
               <li>
-                <Link to="/home">Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li>
                 <Link to="/search">Search</Link>
@@ -21,8 +23,9 @@ class App extends Component {
                 <Link to="/create">Create</Link>
               </li>
             </ul>
+            <SearchBar placeholder="Enter a place name..."/>
            <Routes>
-                 <Route exact path='/home' element={< Home />}></Route>
+                 <Route exact path='/' element={< Home />}></Route>
                  <Route exact path='/search' element={< Search />}></Route>
                  <Route exact path='/create' element={< Create />}></Route>
           </Routes>
