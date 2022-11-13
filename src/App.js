b7dd134f75acfed9a789805d9440598f4c7a444f
mlandauro <micaela.landauro@gmail.com>
2022-11-13 01:20:15 -0600

    added home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './component/home';
 import Search from './component/search';
-import User from './component/user';
+import Create from './component/create';
 import './App.css';
   
 class App extends Component {
@@ -11,25 +11,20 @@ class App extends Component {
        <Router>
            <div className="App">
             <ul className="App-header">
-              {/*
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/home">Home</Link>
               </li>
-              */}
-              
               <li>
-                <Link to="/search">Browse</Link>
+                <Link to="/search">Search</Link>
               </li>
-
-              {/*
               <li>
-                <Link to="/user">User</Link>
+                <Link to="/create">Create</Link>
               </li>
             </ul>
            <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/home' element={< Home />}></Route>
                  <Route exact path='/search' element={< Search />}></Route>
-                 <Route exact path='/user' element={< User />}></Route>
+                 <Route exact path='/create' element={< Create />}></Route>
           </Routes>
           </div>
        </Router>
