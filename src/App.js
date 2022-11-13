861d3d0ca74f5ef7dd925d24a5d5ae11cbe99a2b
ZaneLeBlanc <zane.m.leblanc@gmail.com>
2022-11-12 21:55:09 -0600

    added create files and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './component/home';
 import Search from './component/search';
 import User from './component/user';
+import Create from './component/create';
 import './App.css';
   
 class App extends Component {
@@ -11,20 +12,31 @@ class App extends Component {
        <Router>
            <div className="App">
             <ul className="App-header">
+              {/*
               <li>
                 <Link to="/">Home</Link>
               </li>
+              */}
+              
               <li>
-                <Link to="/search">Search</Link>
+                <Link to="/search">Browse</Link>
               </li>
+
+              {/*
               <li>
                 <Link to="/user">User</Link>
+              </li>
+              */}
+
+              <li>
+                <Link to="/create">Create</Link>
               </li>
             </ul>
            <Routes>
                  <Route exact path='/' element={< Home />}></Route>
                  <Route exact path='/search' element={< Search />}></Route>
                  <Route exact path='/user' element={< User />}></Route>
+                 <Route exact path='/create' element={< Create />}></Route>
           </Routes>
           </div>
        </Router>
---
+++ src/component/create.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Create.css';
+
+function Create(){
+    return <>
+    
+    <div id="title">Create Event</div>
+    <div class="subApp">
+        <div class="item">Day</div>
+        <div class="item">Time</div>
+        <div class="item">Group Size</div>
+        <div class="item">Location</div>
+    </div>
+    <button>Submit</button>
+    </>
+}
+
+export default Create;
