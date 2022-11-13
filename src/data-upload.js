a9308feeb2b09f69ce876963cddba95cf315a543
Edgar Andrade <andradeaedgar@gmail.com>
2022-11-13 07:08:59 -0600

    update to create

--- src/data-upload.js
+++ src/data-upload.js
@@ -5,6 +5,9 @@ const key = process.env.GOOGLE_API_KEY='AIzaSyCrFk5QukjYj508-4hmMa3QO4QPFeHz4RA'
 var axios = require('axios');
 const router = require('express').Router()
 
+var obj = new Array(15);
+
+
 var config = {
   method: 'get',
   url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyCrFk5QukjYj508-4hmMa3QO4QPFeHz4RA',
@@ -16,9 +19,11 @@ axios(config)
   const file = JSON.stringify(response.data);
   var text = JSON.parse(file);
   for (var i = 0; i < text.results.length; i++) {
-    var counter = text.results[i];
-    console.log(counter.name);
+    obj[i] = text.results[i];
+    
 }
+
+
 })
 
 .catch(function (error) {
