2b4f0e0f242844ae237f27cd2c765e747a874a9c
ZaneLeBlanc <zane.m.leblanc@gmail.com>
2022-11-13 01:21:40 -0600

    personal branch commit with updated create files

--- src/component/browse.js
+++ src/component/browse.js
@@ -1,13 +1,13 @@
 import React from "react";
   
-const Search = () => {
+const Browse = () => {
   return (
     <div>
       <h1>
-        This is the search page
+        This is the Browse page
       </h1>
     </div>
   );
 };
   
-export default Search;
+export default Browse;
--- src/component/create.js
+++ src/component/create.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import './Create.css';
 
+
 function Create(){
     return <>
     
-    <div id="title">Create Event</div>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=UnifrakturMaguntia&display=swap');
+    </style>
+    <div id="title" ><strong>Create Event</strong></div>
     <div class="subApp">
-        <div class="item">Day</div>
-        <div class="item">Time</div>
-        <div class="item">Group Size</div>
-        <div class="item">Location</div>
+        
+        <div class="item"><strong>Day</strong>
+            <fieldset class='legacy-form-row'>
+            <input id='date_input'
+                    name='talk-type'
+                    type='radio'
+                    value='main-stage' />
+            <label for='talk-type-1' class='radio-label'>Today</label>
+            
+            <input id='talk-type-2'
+                    name='talk-type'
+                    type='radio'
+                    value='workshop'
+                    checked />
+            <label for='talk-type-2' class='radio-label'>Tomorrow</label>
+
+            <input id='talk-type-3'
+                    name='talk-type'
+                    type='radio'
+                    value='workshop'
+                    checked />
+            <label for='talk-type-3' class='radio-label'>11/15</label>
+
+            <input id='talk-type-3'
+                    name='talk-type'
+                    type='radio'
+                    value='workshop'
+                    checked />
+            <label for='talk-type-3' class='radio-label'>11/16</label>
+
+            <input id='talk-type-3'
+                    name='talk-type'
+                    type='radio'
+                    value='workshop'
+                    checked />  
+            <label for='talk-type-3' class='radio-label'>11/17</label>
+            </fieldset>
+        </div>
+        <div id="separator"></div>
+
+        <div class="item"><strong>Time</strong></div>
+        <div id="separator"></div>
+
+        <div class="item"><strong>Group Size</strong></div>
+        <div id="separator"></div>
+
+        <div class="item"><strong>Location</strong></div>
+
     </div>
     <button>Submit</button>
     </>
